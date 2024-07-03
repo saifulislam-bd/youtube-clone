27ae7c0d7f6335dd463605640d7b2bd81ac595d3
saifulislam-bd <saifulislam.github@gmail.com>
2024-07-03 21:13:46 +0600

    add video from youtube api in the feed

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,113 +1,43 @@
 import "./Feed.css";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
 import { Link } from "react-router-dom";
+import { API_KEY, value_converter } from "../../utils/data.js";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
-const Feed = () => {
+const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=BD&videoCategoryId=${category}&key=${API_KEY}`;
+
+    await fetch(videoList_url)
+      .then((res) => res.json)
+      .then((data) => setData(data.items));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [category]);
+
   return (
     <div className="feed">
-      <Link to={`video/15/11`} className="card">
-        <img src={thumbnail1} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </Link>
-      <div className="card">
-        <img src={thumbnail2} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail3} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail4} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail5} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail6} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail7} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail8} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail1} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail2} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail3} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail4} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail5} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail6} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail7} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail8} alt="thumbnail" />
-        <h2>Best channel to learn web development</h2>
-        <h3>Thapa Technical</h3>
-        <p>15k views • 2 days ago</p>
-      </div>
+      {data?.map((item, idx) => {
+        return (
+          <Link
+            to={`video/${item.snippet.categoryId}/${item.id}`}
+            className="card"
+            key={idx}
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="thumbnail" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>
+              {value_converter(item.statistics.viewCount)} views •
+              {moment(item.snippet.publishedAt).fromNow()}
+            </p>
+          </Link>
+        );
+      })}
     </div>
   );
 };
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,1 +1,11 @@
 export const API_KEY = "AIzaSyCybcymSY1kDeXRO1Wjrz1fjv2C26N0hwc";
+
+export const value_converter = (value) => {
+  if (value >= 1000000) {
+    return Math.floor(value / 1000000) + "M";
+  } else if (value >= 1000) {
+    return Math.floor(value / 1000) + "K";
+  } else {
+    return value;
+  }
+};
